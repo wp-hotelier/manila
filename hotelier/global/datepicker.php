1d66a11eb9b310bac38ef03716d583f14bd3effd
Benito Lopez <hello@lopezb.com>
2021-12-02 17:18:03 +0100

    Hide datepicker inputs

--- hotelier/global/datepicker.php
+++ hotelier/global/datepicker.php
@@ -23,8 +23,8 @@ $datepicker_form_url = apply_filters( 'hotelier_datepicker_form_url', HTL()->car
 	<span class="hotelier_datepicker__label"><?php esc_html_e( 'Pick your dates', 'manila' ); ?></span>
 
 	<span class="datepicker-input-select-wrapper"><input class="datepicker-input-select" type="text" id="hotelier-datepicker-select" value=""></span>
-	<input type="text" id="hotelier-datepicker-checkin" class="datepicker-input datepicker-input--checkin" name="checkin" value="<?php echo esc_attr( $checkin ); ?>">
-	<input type="text" id="hotelier-datepicker-checkout" class="datepicker-input datepicker-input--checkout" name="checkout" value="<?php echo esc_attr( $checkout ); ?>">
+	<input type="text" id="hotelier-datepicker-checkin" class="datepicker-input datepicker-input--checkin" name="checkin" value="<?php echo esc_attr( $checkin ); ?>" style="display: none;">
+	<input type="text" id="hotelier-datepicker-checkout" class="datepicker-input datepicker-input--checkout" name="checkout" value="<?php echo esc_attr( $checkout ); ?>" style="display: none;">
 
 	<?php echo apply_filters( 'hotelier_datepicker_button_html', '<input type="submit" class="button button--datepicker" name="hotelier_datepicker_button" id="datepicker-button" value="' . esc_attr__( 'Book Now', 'manila' ) . '" />' ); ?>
 </form>
